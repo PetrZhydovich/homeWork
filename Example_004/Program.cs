3b01f0acb50180e9f3c375f7265a4aedf2ac2e69
Petr <povar_cr@mail.ru>
2022-02-09 21:27:17 +0300

    add Example_004

---
+++ Example_004/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать четные числа от 1 до N
+Console.WriteLine("Введите число N: ");
+string strN = Console.ReadLine();
+int num = int.Parse(strN);
+int min = 2;
+while (min < num)
+{
+    if (min % 2 == 0) Console.WriteLine($"Четные числа: {min} ");
+    min++;
+}
