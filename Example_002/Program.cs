bdb38a5b986b41d6e7efa2baff07e25042827e1f
Petr <povar_cr@mail.ru>
2022-02-09 20:40:48 +0300

    add Example_002

---
+++ Example_002/Program.cs
@@ -0,0 +1,15 @@
+﻿//Найти максимальное из трех чисел
+int numbreOne = new Random().Next(1,99);
+Console.WriteLine("Перовое число: " + numbreOne);
+
+int numbreTwo = new Random().Next(1,99);
+Console.WriteLine("Второе число: " + numbreTwo);
+
+int numbreThree = new Random().Next(1,99);
+Console.WriteLine("Третье число: " + numbreThree);
+
+int maxNumber = numbreOne;
+
+if (maxNumber < numbreTwo) maxNumber = numbreTwo;
+if (maxNumber < numbreThree) maxNumber = numbreThree;
+Console.WriteLine("Максимальное из трёх чисел: " + maxNumber );
