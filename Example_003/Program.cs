2aae75b8b7d611391a684a9a8ebed76b56629b5c
Petr <povar_cr@mail.ru>
2022-02-09 20:57:33 +0300

    add Example_003

---
+++ Example_003/Program.cs
@@ -0,0 +1,10 @@
+﻿//  Выяснить является ли число чётным
+Console.WriteLine("Введите целое число: ");
+string numberStr = Console.ReadLine();
+ double num = int.Parse(numberStr);
+ if (num % 2 == 0) Console.WriteLine($"Число {num} Является четным");
+ else
+ {
+     Console.WriteLine($"Число {num} не является четным");
+ }
+ 
